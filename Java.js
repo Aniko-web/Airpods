c77eea77c18c504a2956eb11392c9c37532e9e36
Aniko <anikosanuno@gmail.com>
2026-03-27 00:28:30 +0500

    first commit

--- Java.js
+++
@@ -1,8 +0,0 @@
-let header_img = document.querySelectorAll(".header_img");
-let i = 0;
-
-setInterval(() => {
-    header_img[i].classList.remove("active");
-    i = (i + 1) % header_img.length;
-    header_img[i].classList.add("active");
-}, 3000);
